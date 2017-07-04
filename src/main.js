922e0da5ed7c6bf0a191aaa1cbfc4d235a5f3a95
caoyu412 <275125992@qq.com>
2017-07-04 15:06:18 +0800

    fix: change the project config

--- src/main.js
+++ src/main.js
@@ -1,19 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import VRouter from './router'
-// import Vuex from 'vuex'
+import Layout from './components/Layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
 
 Vue.config.productionTip = false
 
-Vue.use(VRouter)
-// Vue.use(Vuex);
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  VRouter,
-  // Vuex,
-  template: '<App/>',
-  components: { App }
+  router,
+  template: '<Layout/>',
+  components: { Layout }
 })
